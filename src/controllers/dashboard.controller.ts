12033bd5a338cccfd2f6065d56daa22a1c3ec1f6
KY SOPHEAREAK <reakphea0802@gmail.com>
2025-06-18 22:21:35 +0700

    update

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -67,3 +67,77 @@ export const getDashboardStats = async (req: Request, res: Response) => {
     ResponeUtil.fail(res, 500, "Error calculating revenue", error);
   }
 };
+
+export const getDashboardProductStatus = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $addFields: {
+          totalAmount: { $multiply: ["$price", "$stock"] }
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          totalInventoryValue: { $sum: "$totalAmount" },
+          totalProducts: { $sum: 1 },
+          totalStock: { $sum: "$stock" }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          totalInventoryValue: 1,
+          totalProducts: 1,
+          totalStock: 1
+        }
+      }
+    ]);
+
+    const favoriteProducts = await Product.aggregate([
+      {
+        $match: {
+          "reviews.rating": 5
+        }
+      },
+      {
+        $project: {
+          title: 1,
+          hasFiveStarReview: {
+            $gt: [
+              {
+                $size: {
+                  $filter: {
+                    input: "$reviews",
+                    as: "review",
+                    cond: { $eq: ["$$review.rating", 5] }
+                  }
+                }
+              },
+              0
+            ]
+          }
+        }
+      },
+      {
+        $match: {
+          hasFiveStarReview: true
+        }
+      },
+      {
+        $count: "totalFavoriteItems"
+      }
+    ]);
+
+    let responseData: any [] = [
+      favoriteProducts[0],
+      products[0]
+    ];
+
+
+    ResponeUtil.success(res, responseData);
+  } catch (error) {
+    console.error("=====> Error fetching dashboard product status:", error);
+    ResponeUtil.fail(res, 500, "Internal Server Error", "An error occurred while fetching product status");
+  }
+};
--- src/routes/dashboard.route.ts
+++ src/routes/dashboard.route.ts
@@ -1,5 +1,7 @@
 import { getDashboardStats } from "../controllers/dashboard.controller";
+import { getDashboardProductStatus } from "../controllers/dashboard.controller";
 import { Request, Response } from "express";
+import ResponseUtil from "../utils/ResponseUtil";
 
 export default [
   {
@@ -23,4 +25,17 @@ export default [
       getDashboardStats(req, res);
     },
   },
+
+  {
+    path: "/dashboard/product/stats",
+    method: "get",
+    handler: async (req: Request, res: Response) => {
+      try {
+        getDashboardProductStatus(req, res);
+      } catch (err: any) {
+        console.error("=====> Error fetching dashboard product status:", err);
+        ResponseUtil.fail(res, 500, "Internal Server Error", "An error occurred while fetching product status");
+      }
+    }
+  }
 ];
